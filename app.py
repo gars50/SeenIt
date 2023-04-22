a40febfb80b86b12f2e42e2f3d97f17f44103394
unknown <julienrossignol@gmail.com>
2023-04-22 00:32:16 -0400

    Added config files + default config

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-import os
-import datetime
+from markupsafe import escape
 from flask import Flask
+from configparser import ConfigParser
 
 app = Flask(__name__)
+
+config = ConfigParser()
+config.read("config.ini")
+
+testName = config.get("DEFAULT", "testName")
 
 @app.route("/")
 def index():
     return "<h1>Hello!</h1>"
+
+@app.route("/test")
+def test():
+    return '<h1>{}</h1>'.format(escape(testName.capitalize()))
 
 
 #Run with
