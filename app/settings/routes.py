631fd0dc5d54afa3df7f94cddf41e80015ef87a1
Julien Rossignol <julienrossignol@gmail.com>
2023-05-16 21:07:38 -0400

    Added cust CSS, Users page, Cleaned up default.py

--- app/settings/routes.py
+++ app/settings/routes.py
@@ -12,6 +12,11 @@ def users():
     users = User.query.all()
     return render_template('settings/users.html', users=users)
 
+@bp.route('/user/<int:user_id>')
+def user(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('settings/user.html', user=user)
+
 @bp.route('/connections')
 def connections():
     return render_template('settings/connections.html')
--- default.py
+++ default.py
@@ -1,21 +1,5 @@
-#Generate this key with os.urandom(24).hex()
-SECRET_KEY = "xxxxxx"
-FLASK_APP = "SeenIt"
-ENVIRONMENT = "production"
-
-#Ombi information
-ombi_host = "xxxxx"
-ombi_port = 5000
-ombi_apikey = "xxxxxx"
-
-#Radarr information
-radarr_host = "win-unit"
-radarr_port = 7878
-radarr_apikey = "xxxxxx"
-
-#Sonarr information
-sonarr_host = "win-unit"
-sonarr_port = 8989
-sonarr_apikey = "xxxxxx"
-
-#SQL information
+class Config:
+    #Generate this key with os.urandom(24).hex()
+    SECRET_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    SQLALCHEMY_DATABASE_URI = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
