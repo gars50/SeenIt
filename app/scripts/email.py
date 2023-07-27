fdedf2d934fbf8ebd72b05d7e1c75f9b480c6d84
Julien Rossignol <julienrossignol@gmail.com>
2023-07-26 20:15:00 -0400

    Few fixes before prod

--- app/scripts/email.py
+++ app/scripts/email.py
@@ -18,7 +18,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email('[SeenIt] Reset Your Password',
-               sender=current_app.config["SENDER"],
+               sender=current_app.config["MAIL_USERNAME"],
                recipients=[user.email],
                text_body=render_template('auth/email_reset_password.txt', user=user, token=token),
                html_body=render_template('auth/email_reset_password.html', user=user, token=token))
