e1cfa91a3faf1230d91d778127c6dedd4825b298
Julien Rossignol <julienrossignol@gmail.com>
2023-07-27 19:49:46 -0400

    Fixed bug with logger in job

--- app/scripts/jobs.py
+++ app/scripts/jobs.py
@@ -1,13 +1,12 @@
 from app import scheduler
-from flask import current_app
 from app.scripts.media import import_requests_from_ombi, update_media_infos
 
 def update_medias_and_requests():
-    current_app.logger.info("Running an update on all medias.")
     with scheduler.app.app_context():
+        scheduler.app.logger.info("Running an update on all medias.")
         #Import new requests from Ombi
         import_requests_from_ombi()
         #Update media infos from Sonarr/Radarr
         update_media_infos()
-    current_app.logger.info("Update completed.")
+        scheduler.app.logger.info("Update completed.")
     #Maybe import Medias from Sonarr/Radarr?
